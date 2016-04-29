28796d30d5fd55940320d4ab2d90affb12715cda
Alex Mishin <mishin1990@gmail.com>
2016-04-29 10:40:38 +0300

    Initial Structure
    just wrote down the main methods needed for the assignment

---
+++ QuickSortTrace/QuickSortTrace.java
@@ -0,0 +1,47 @@
+
+/**
+ * 
+ * 
+ * @author Nir & Alex 
+ * @version 0.1
+ */
+public class QuickSortTrace
+{
+    // instance variables
+    private int _n;
+    private int _A[];
+
+    /**
+     * A constructor that build an array with n+1 unique int values,
+     * from index 1 to n+1 (A[0] not used
+     */
+    public QuickSortTrace(int n)
+    {
+
+    }
+
+    /**
+     * Find the index of the k-th element in array A,
+     * for example, k=1 will return the index of the minimal element.
+     * 
+     * @param k     k-th element to return from smallest to largest
+     * @return      the index of the k-th element
+     */
+    public int RandomizedSelect(int k)
+    {
+        //inside note: be careful not to change the original array A
+        return 0;
+    }
+
+    /**
+     * Sorts the array A using the QuickSort algorithem, 
+     * whilst selecting the pivot element to be the k-th element of the array
+     * 
+     * @param k     k-th element of the array to use as pivot for QuickSort
+     * @return      maximum recursion depth   
+     */
+    public int QuickSortStackTrace(int k)
+    {
+        return 0;
+    }
+}
