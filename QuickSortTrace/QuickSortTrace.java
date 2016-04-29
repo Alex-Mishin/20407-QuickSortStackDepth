d5568a6dff76e4e23c5d58e6647a2de94bddd0da
Alex Mishin <mishin1990@gmail.com>
2016-04-29 12:53:07 +0300

    Constructor
    Finished the constructor, it generates numbers from 1 to n, and then
    shuffles them randomly

--- QuickSortTrace/QuickSortTrace.java
+++ QuickSortTrace/QuickSortTrace.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 /**
  * 
@@ -7,19 +8,39 @@
  */
 public class QuickSortTrace
 {
+    
+    
     // instance variables
+    private int[] _A;
     private int _n;
-    private int _A[];
+    
 
     /**
      * A constructor that build an array with n+1 unique int values,
-     * from index 1 to n+1 (A[0] not used
+     * from index 1 to n+1 (A[0] not used)
      */
     public QuickSortTrace(int n)
     {
-
+        _A = new int[n+1];
+        _n = n;
+        for (int i = 1; i <= _n; i++) //fill array with numbers from 1 to n
+            _A[i] = i;
+        Random rand = new Random();
+        for (int i = 1; i <= _n; i++) //shuffle array
+            arraySwap(_A[i], rand.nextInt(_n)+1);
+        for (int i = 0; i <= _n; i++) //print array
+            System.out.println(i + ": "+ _A[i]);
+    
     }
 
+    private void arraySwap(int i ,int j)
+    {
+        int temp = _A[i];
+        _A[i] = _A[j];
+        _A[j] = temp;
+    }
+    
+    
     /**
      * Find the index of the k-th element in array A,
      * for example, k=1 will return the index of the minimal element.
@@ -27,11 +48,12 @@ public class QuickSortTrace
      * @param k     k-th element to return from smallest to largest
      * @return      the index of the k-th element
      */
-    public int RandomizedSelect(int k)
+    public int randomizedSelect(int k)
     {
         //inside note: be careful not to change the original array A
         return 0;
     }
+    
 
     /**
      * Sorts the array A using the QuickSort algorithem, 
@@ -40,7 +62,7 @@ public class QuickSortTrace
      * @param k     k-th element of the array to use as pivot for QuickSort
      * @return      maximum recursion depth   
      */
-    public int QuickSortStackTrace(int k)
+    public int quickSortStackTrace(int k)
     {
         return 0;
     }
